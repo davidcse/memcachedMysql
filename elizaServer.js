e64073274a2dea46a985454d03770bb338cb68e0
David Lin <david.lin.cse@gmail.com>
2017-02-05 20:41:26 -0500

    fixed issue with serving static files via express js, solution is to add the public folder and use app.use('/public',express.static('public'))

--- elizaServer.js
+++ elizaServer.js
@@ -1,17 +1,19 @@
 /*
   Server program for Eliza at davilin.cse356.cs.compas.stonybrook.edu/eliza
 */
-var app = require('express')();
+var express = require('express');
+var app = express();
 var fs = require("fs");
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static('public'))
 app.set('view engine', 'pug')
 app.set('views','./views')
 
 
 app.get('/eliza', function (req, res) {
-   fs.readFile( __dirname + "/" + "eliza_home.html", 'utf8', function (err, data) {
+   fs.readFile( __dirname + "/" + "public/eliza_home.html", 'utf8', function (err, data) {
        res.end( data );
    });
 });
@@ -22,23 +24,12 @@ app.post('/eliza', function (req, res) {
     //if name field was supplied, use pug template engine to generate authenticated page
     res.render('authenticated', {name: req.body.name, date:Date()});
   }else{
-    fs.readFile( __dirname + "/" + "eliza_error.html", 'utf8', function (err, data) {
+    fs.readFile( __dirname + "/" + "public/eliza_error.html", 'utf8', function (err, data) {
       res.end(data);
     });
   }
 });
 
-app.get('/css/eliza-style.css', function (req, res) {
-   fs.readFile( __dirname + "/" + "css/eliza-style.css", 'utf8', function (err, data) {
-       res.end( data );
-   });
-});
-
-app.get('/js/therapySession.js', function (req, res) {
-   fs.readFile( __dirname + "/" + "js/therapySession.js", 'utf8', function (err, data) {
-       res.end(data);
-   });
-})
 
 // Starts the server
 var server = app.listen(9000, function () {
