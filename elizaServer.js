7a697d19223fe1541ae44c2eec32f73f9037d5b7
David Lin <david.lin.cse@gmail.com>
2017-02-07 00:47:49 -0500

    added more eliza responses

--- elizaServer.js
+++ elizaServer.js
@@ -7,10 +7,43 @@ var fs = require("fs");
 var bodyParser = require('body-parser');
 
 var responses = [
-  'Hi how are you',
   'Now why do you say that?',
   "Let's dig deeper into why that is the case",
-  "Excellent observation"
+  "Excellent observation",
+  "Let's expand on that a little further",
+  "I'm not quite sure of what you said",
+  "Tell me about your problems", 
+  "Go ahead, I'm listening",
+  "Hmmm... I see",
+  "What if we try talking a little more about this",
+  "I'm very glad to be talking to you",
+  "Now why don't you tell me more",
+  "Life is hard, I understand",
+  "I can help you with your problems",
+  "I believe what you're saying",
+  "It wasn't your fault",
+  "I'm very sorry",
+  "You're okay just as your are",
+  "I won't send you away",
+  "You are precious",
+  "I trust you and what you say",
+  "Talk to me, I won't hurt you",
+  "I like you",
+  "I feel what you're saying",
+  "You can choose what you want to share with me",
+  "It's okay to cry, we all do it sometimes",
+  "So tell me more about it",
+  "Let's revisit that a little later",
+  "I have no interest in telling you what to do",
+  "Let's talk about your childhood",
+  "Why don't we talk about what was your earliest memory?",
+  "What are you thinking right now?",
+  "Is that what you expected to happen?",
+  "What would you do if I wasn't here?",
+  "What you just said before, is a step in the right direction",
+  "You know, my mamma always told me that life is like a box of chocolates",
+  "Go on, I'm listening to you right now",
+  "I can't answer that right now, but let's get back to it later"
 ]
 
 function getRandomResponse() {
@@ -25,10 +58,16 @@ app.set('view engine', 'pug')
 app.set('views','./views')
 
 app.post('/eliza/DOCTOR', function (req, res) {
-  console.log("POST DOCTOR request");
   console.log(req.body);
-  if(req.body.human){
-    res.end(JSON.stringify({'eliza':getRandomResponse()}));
+  if(req.body.human && typeof(req.body.human)=='string'){
+    var input = req.body.human.toLowerCase();
+    var output;
+    if(input.includes("hi ") || input.includes("hello ") || input.includes("hey ")){
+      output = 'Hi how are you?';
+    }else{
+      output = getRandomResponse();
+    }
+    res.end(JSON.stringify({'eliza':output}));
   }else{
     //invalid format, expected field 'human' in request's json
     res.end("you sent wrong format");
