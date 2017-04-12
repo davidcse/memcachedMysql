28e940156fbc1bf11ec2d03a5e60a224e867eb34
David Lin <david.lin.cse@gmail.com>
2017-04-11 22:59:31 -0400

    test memcache

--- memcacheServer.js
+++ memcacheServer.js
@@ -8,7 +8,14 @@ var app = express();
 var fs = require("fs");
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
-
+var Memcached = require('memcached');
+var memcached = new Memcached();
+memcached.connect('127.0.0.1:11211',function(err,conn){
+  if(err){
+    console.log("memcached error starting");
+    console.log(conn.server);
+  }
+});
 
 //APP CONFIG
 app.use(bodyParser.urlencoded({extended : true}));
@@ -28,50 +35,60 @@ var pool=mysql.createPool({
 
 function handle_database(state,service_type, res) {
     //console.log("servicing request - state " + state + "\t service_type : "  + service_type);
-    pool.getConnection(function(err,connection){
-        if (err) {
-          return res.json({
-            "status" : "ERROR",
-            "message" : "Error in connection database"
-         });
-        }
-        //console.log('connected as id ' + connection.threadId);
+    var queryString = "select AVG(comm_rate) AS comm_rate_avg, " +
+    "AVG(ind_rate) AS ind_rate_avg, " +
+    "AVG(res_rate) AS res_rate_avg " +
+    "from electric  " +
+    "where state='" + state+"' " +
+    "AND service_type='"+service_type+"';";
+    memcached.get(queryString,function(err,data){
+        if(data){
+          return res.json(data);
+        }else{
+            pool.getConnection(function(err,connection){
+                if (err) {
+                  return res.json({
+                    "status" : "ERROR",
+                    "message" : "Error in connection database"
+                 });
+                }
+                //console.log('connected as id ' + connection.threadId);
+                connection.query(queryString,function(err,rows){
+                    connection.release();
+                    if(!err) {
+                      //console.log("Rows : " + JSON.stringify(rows));
+                      if(rows){
+                        var responseObj = {
+                          "status": "OK",
+                          "comm_rate_avg": rows[0].comm_rate_avg,
+                          "ind_rate_avg": rows[0].ind_rate_avg,
+                          "res_rate_avg": rows[0].res_rate_avg
+                        };
+                        memcached.set(queryString,responseObj,100000,function(err){
+                          if(err){
+                            console.log("memcached set error");
+                          }
+                        });
+                        return res.json(responseObj);
+                      }
+                    }else{
+            //console.log("Encountered error in result set: "+ err);
+                        return res.json({
+                          "status": "ERROR",
+                          "message" : "No rows"
+                        });
 
-        var queryString = "select AVG(comm_rate) AS comm_rate_avg, " +
-        "AVG(ind_rate) AS ind_rate_avg, " + 
-        "AVG(res_rate) AS res_rate_avg " + 
-        "from electric  " + 
-        "where state='" + state+"' " +
-        "AND service_type='"+service_type+"';";
-
-        connection.query(queryString,function(err,rows){
-            connection.release();
-            if(!err) {
-              //console.log("Rows : " + JSON.stringify(rows));
-              if(rows){
-                return res.json({
-                  "status": "OK",
-                  "comm_rate_avg": rows[0].comm_rate_avg,
-                  "ind_rate_avg": rows[0].ind_rate_avg,
-                  "res_rate_avg": rows[0].res_rate_avg
+                    }
                 });
-              }
-            }else{
-		//console.log("Encountered error in result set: "+ err);
-                return res.json({
-                  "status": "ERROR",
-                  "message" : "No rows"
+                connection.on('error', function(err) {
+                  return res.json({
+                    "status" : "ERROR",
+                    "message" : "Error in connection database"
+                  });
                 });
-                
-	    }
-        });
-        connection.on('error', function(err) {
-          return res.json({
-            "status" : "ERROR",
-            "message" : "Error in connection database"
           });
-        });
-  });
+        }
+    });
 }
 
 app.post('/hw7', function (req, res) {
